b3d076f52651f5a903fbb456ecf858556408f514
Muhammadjon <167974467+Muhammadjon141@users.noreply.github.com>
2024-05-31 18:25:59 +0500

    Add files via upload

---
+++ bibary search.py
@@ -0,0 +1,44 @@
+
+
+def check_list():
+    datas = input("Listga ma'lumotlar kiriting(son): ").split()
+    dat = []
+    for data in datas:
+        try:       
+            if data.isdigit():
+                for i in datas:
+                    dat.append(float(i))
+                return binary_search(dat)
+            else:
+                print("Faqat son kiriting! ")
+                return check_list()
+        except:
+            print("Faqat son kiriting! ")
+            return check_list()
+
+
+def binary_search(dat):
+    try:
+        search = int(input("Qidirayotgan sonni kiriting: "))
+        low = 0
+        data_last = len(dat)
+        midle = (data_last + low) // 2
+        count = 0
+        while True:
+            count += 1
+            if dat[midle] > search:
+                data_last = midle
+            elif dat[midle] < search:
+                low = midle
+            elif dat[midle] == search:
+                print(dat[midle], count)
+                break
+            else:
+                print("Bunday element mavjud emas!")
+                return binary_search(dat)
+    except:
+        print("Son kiriting! ")
+        return binary_search(dat)
+
+if __name__ == "__main__":
+    check_list()
