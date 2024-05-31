e0636651880162ca0bc9904084a6ebaa7e8c8a32
Muhammadjon <167974467+Muhammadjon141@users.noreply.github.com>
2024-05-31 18:58:15 +0500

    Add files via upload

---
+++ liner_search.py
@@ -0,0 +1,14 @@
+import random
+
+data = []
+for i in range(30):
+    data.append(random.randint(1, 100))
+print(data)
+son = int(input("Sonni kiriting: "))
+count = 0
+for j in data:
+    count += 1
+    if j == son:
+        print(son)
+        break
+print(f"Urunishalr soni: {count}")
